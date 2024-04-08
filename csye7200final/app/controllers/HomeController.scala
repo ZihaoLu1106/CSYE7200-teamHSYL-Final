42a7b8293f43cf4799193b221362c072a94f6181
ZihaoLu1106 <lu.ziha@northeastern.edu>
2024-04-08 19:30:21 -0400

    add mongodb connect

--- csye7200final/app/controllers/HomeController.scala
+++ csye7200final/app/controllers/HomeController.scala
@@ -20,5 +20,6 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
    */
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
+
   }
 }
