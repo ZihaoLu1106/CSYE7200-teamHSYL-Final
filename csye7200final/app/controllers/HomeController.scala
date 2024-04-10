36c65aa4ee1aafaea1c37471215932f43243d485
ZihaoLu1106 <lu.ziha@northeastern.edu>
2024-04-10 17:31:33 -0400

    update

--- csye7200final/app/controllers/HomeController.scala
+++ csye7200final/app/controllers/HomeController.scala
@@ -27,8 +27,9 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
 
   }
   def about() = Action { implicit request: Request[AnyContent] =>
-    val source = Source.fromFile("app/data/df.csv")
+    val source = Source.fromFile("app/data/data.csv")
     val data = try {
+      println("nmd2")
       source.getLines().drop(1).map { line =>
         val Array(age, bmiCategory, bloodPressure, gender, physicalActivityLevel, qualityOfSleep, sleepDuration, stressLevel, id) = line.split(",").map(_.trim)
         CSVRow(age.toInt, bmiCategory.toInt, bloodPressure.toInt, gender.toDouble, physicalActivityLevel.toDouble, qualityOfSleep.toInt, sleepDuration.toDouble, stressLevel.toInt, id)
@@ -39,13 +40,18 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
     Ok(views.html.about(data)) // Renders the about view
   }
   def processInputs() = Action { implicit request: Request[AnyContent] =>
-    val input1 = request.body.asFormUrlEncoded.get("input1").head
-    val input2 = request.body.asFormUrlEncoded.get("input2").head
-    val input3 = request.body.asFormUrlEncoded.get("input3").head
-
-    // Do something with the input values
-    println(s"Input 1: $input1, Input 2: $input2, Input 3: $input3")
-    Ok(s"Input 1: $input1, Input 2: $input2, Input 3: $input3")
+    val formData = request.body.asFormUrlEncoded
+    val input1Option = formData.flatMap(_.get("input1").flatMap(_.headOption))
+    val input2Option = formData.flatMap(_.get("input2").flatMap(_.headOption))
+    val input3Option = formData.flatMap(_.get("input3").flatMap(_.headOption))
+    println("nmd")
+    (input1Option, input2Option, input3Option) match {
+      case (Some(input1), Some(input2), Some(input3)) =>
+        println(s"Input 1: $input1, Input 2: $input2, Input 3: $input3")
+        Ok(views.html.processInputs(input1, input2, input3))
+      case _ =>
+        BadRequest("Missing input values")
+    }
   }
 }
 
